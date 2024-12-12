71b5a6f6a5b379f69b6d3e6d6962da5bbe947562
labor <labor@home.com>
2024-12-12 22:28:09 +0800

    完美转发

--- hello_cpp/test_forward.cpp
+++ hello_cpp/test_forward.cpp
@@ -2,14 +2,85 @@
  * Auth :   liubo
  * Date :   2023-08-17 00:00:00
  * Comment: 测试std::forward
+ * 详细，看这个，解释的更清楚：  https://www.bilibili.com/video/BV1a1SPYaEBY
+ * 引用折叠规则：
+ *          T& &   -> T&    左值引用 的引用  折叠成 左值引用
+ *          T& &&  -> T&    左值引用 的右值引用  折叠成 左值引用
+ *          T&& &  -> T&    右值引用 的引用  折叠成 左值引用
+ *          T&& && -> T&&   右值引用 的右值引用  折叠成 右值引用
+ *          很有规律，就是带左值引用的，就被折叠成左值引用。不带左值引用的，即全部是右值引用的，就会折叠成右值引用！
  */
 
 #include <iostream>
 #include <string.h>
 #include "AutoRegTestFunc.h"
 
+namespace std_source_code
+{
+    template<typename T>
+    T&& forward(typename std::remove_reference<T>::type& t) noexcept
+    {
+        return static_cast<T&&>(t);
+    }
+
+    // 比如，如果是这个：forward<std::string&>，那么转成源码后，得到的返回值是std::string& &&，那么最终得到的就是std::string&（被折叠了）！ 
+}
+
 namespace test_forward
 {
+    void f(const std::string& s)
+    {        
+        std::cout << "左值：" << s << std::endl;
+    }
+    void f(std::string&& s)
+    {        
+        std::cout << "右值：" << s << std::endl;
+    }
+    
+    void g(const std::string& s)
+    {        
+        f(s);
+    }
+    void g(std::string&& s)
+    {
+        // !!! 看这里！ 问，f(s)调用的是左值还是右值引用？
+        // 答案是，右值引用。因为s是个临时变量，能取地址（即能&s)，所以是左值！
+        f(s);
+#if false
+        void* p = &s;
+        std::cout << "s是能够取地址的！所以是左值！" << p << std::endl;
+#endif
+    }
+
+    
+    void g2(const std::string& s)
+    {        
+        f(s);
+    }
+    void g2(std::string&& s)
+    {
+        // 必须调用forawrd，否则，不会调用f的右值引用！
+        f(std::forward<std::string&&>(s));
+    }
+
+    static void TestForward2()
+    {
+        {
+            g(std::string("hello1")); // 右值引用！因为无法取地址，是即将消亡的值！所有匿名变量，都是右值引用！
+            std::string s2("hello2");
+            g(s2); // 左值！因为能取地址！！
+        }
+
+        // 这个才是正确写法！
+        {
+            g2(std::string("hello1"));
+            std::string s2("hello2");
+            g2(s2); // 左值！因为能取地址！！            
+        }
+    }
+
+
+    
     struct A
     {
         int value;
@@ -117,4 +188,5 @@ namespace test_forward
 
 
     static AutoRegTestFunc autoTestForward(TestForward);
+    static AutoRegTestFunc autoTestForward2(TestForward2);
 }
